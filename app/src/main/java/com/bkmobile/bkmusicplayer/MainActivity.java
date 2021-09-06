8590594bd039fbead688d91496e01efd09bc7048
Berkan Karayilmaz <berkankarayilmaz@gmail.com>
2021-09-06 22:17:06 +0300

    Permission and Custom Adapter

---
+++ app/src/main/java/com/bkmobile/bkmusicplayer/MainActivity.java
@@ -0,0 +1,143 @@
+package com.bkmobile.bkmusicplayer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] items;
+
+    private int storeagePermissionCode=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.listViewSong);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(MainActivity.this, "You Have Already Granted This Permission!", Toast.LENGTH_SHORT).show();
+            displaySongs();
+
+        }
+        else{
+            requestStoragePermission();
+        }
+
+
+    }
+
+private void requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            new AlertDialog.Builder(this).setTitle("Permission Needed").setMessage("This Permission For Read Your Music List")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},storeagePermissionCode);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(MainActivity.this, "Permission Cancelled", Toast.LENGTH_SHORT).show();
+                        }
+                    }).create().show();
+        }
+        else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},storeagePermissionCode);
+        }
+}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == storeagePermissionCode) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                displaySongs();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public ArrayList<File> findSong(File file){
+        ArrayList<File>arrayList=new ArrayList<>();
+        File[] files= file.listFiles();
+
+        for(File singleFile: files){
+            if(singleFile.isDirectory()&&!singleFile.isHidden()){
+                arrayList.addAll(findSong(singleFile));
+            }
+            else{
+                if(singleFile.getName().endsWith(".mp3")||singleFile.getName().endsWith(".wav")){
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    void  displaySongs(){
+        final ArrayList<File> mySongs=findSong(Environment.getExternalStorageDirectory());
+
+        items=new String[mySongs.size()];
+        for(int i=0;i<mySongs.size();i++){
+            items[i]=mySongs.get(i).getName().toString().replace(".mp4","").replace(".wav","");
+        }
+       /* ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(myAdapter);*/
+        customAdapter customAdapter=new customAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+    class customAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View myView=getLayoutInflater().inflate(R.layout.list_item,null);
+            TextView textSong=myView.findViewById(R.id.txtsongname);
+            textSong.setSelected(true);
+            textSong.setText(items[i]);
+
+            return myView;
+        }
+    }
+}
